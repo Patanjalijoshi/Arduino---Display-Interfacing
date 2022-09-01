6187d5f189757cc9d5797db1aa4cd6756c0aefec
Patanjali <joshipattu12@gmail.com>
2022-09-01 12:18:48 +0530

    Update Display_Interfacing_code.c.ino

--- Display_Interfacing_code.c/Display_Interfacing_code.c.ino
+++ Display_Interfacing_code.c/Display_Interfacing_code.c.ino
@@ -1,10 +1,8 @@
-// This example shows various featues of the library for LCD with 16 chars and 2 lines.
-
-#include <Arduino.h>
+// Display interfacing
 #include <Wire.h>
 #include <LiquidCrystal_PCF8574.h>
 
-LiquidCrystal_PCF8574 lcd(0x27);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_PCF8574 lcd(0x27);  
 
 
 
@@ -14,7 +12,7 @@ void setup() {
     lcd.setBacklight(255);
     lcd.home();
     lcd.clear();
-    lcd.print("Hello LCD");
+    lcd.print("HELLO WORLD !!");
    
 }  
 
