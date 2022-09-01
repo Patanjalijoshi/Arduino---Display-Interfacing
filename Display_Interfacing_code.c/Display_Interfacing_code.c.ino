7648483eba6eb8445d07071a01f31e532bbd0e43
Patanjali <joshipattu12@gmail.com>
2022-09-01 14:24:03 +0530

    Update Display_Interfacing_code.c.ino

--- Display_Interfacing_code.c/Display_Interfacing_code.c.ino
+++ Display_Interfacing_code.c/Display_Interfacing_code.c.ino
@@ -1,9 +1,5 @@
 // Display interfacing
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 8c2b0218675cc43a7b8cc7df18f83a051d751dcc
 #include <Wire.h>
 #include <LiquidCrystal_PCF8574.h>
 
@@ -14,7 +10,7 @@ LiquidCrystal_PCF8574 lcd(0x27);
 void setup() {
   
     lcd.begin(16, 2);  // initialize the lcd
-    lcd.setBacklight(255);
+    lcd.setBacklight(225);
     lcd.home();
     lcd.clear();
     lcd.print("HELLO WORLD !!");
